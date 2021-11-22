0ed2712f1c83a8d6c8e18dfbbb97f8a0fc510f07
Mohammad-R-Fayyaz <94065472+Mohammad-R-Fayyaz@users.noreply.github.com>
2021-11-22 17:24:53 +0330

    Update topsis.R

--- topsis.R
+++ topsis.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 
 
-d <- read.csv ("C:\\Users\\Fayyaz\\OneDrive\\Applications\\CV & Email\\Canada\\U of T\\Prof. Samin Aref\\analysis\\topsis\\topsis100.csv")
+d <- read.csv ("C:\\Users\\analysis\\topsis\\topsis100.csv")
 d1 <- as.matrix(d) 
 
 w <- c(1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 1)
@@ -9,11 +9,4 @@ i <- c("-", "-", "+", "-", "+", "-", "-", "+", "+", "+", "+", "+")
 
 t <- topsis(d1, w, i)
 t
-write.csv(t,"C:\\Users\\Fayyaz\\OneDrive\\Applications\\CV & Email\\Canada\\U of T\\Prof. Samin Aref\\analysis\\topsis\\topsis10000.csv", row.names = FALSE)
-
-
-
-d <- matrix(rpois(12, 5), nrow = 4)
-w <- c(1, 1, 2)
-i <- c("+", "-", "+")
-topsis(d, w, i)
+write.csv(t,"C:\\Users\\analysis\\topsis\\topsis10000.csv", row.names = FALSE)
